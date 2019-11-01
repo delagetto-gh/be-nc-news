c714fb3c5df6008655e741686f0662cbc3b2c5b3
Eamon Sharifi <eamon_s@hotmail.co.uk>
2019-11-01 15:24:40 +0000

    broken links fixed

--- db/data/development-data/users.js
+++ db/data/development-data/users.js
@@ -2,31 +2,35 @@ module.exports = [
   {
     username: 'tickle122',
     name: 'Tom Tickle',
-    avatar_url: 'https://www.spiritsurfers.net/monastery/wp-content/uploads/_41500270_mrtickle.jpg',
+    avatar_url:
+      'https://www.spiritsurfers.net/monastery/wp-content/uploads/_41500270_mrtickle.jpg'
   },
   {
     username: 'grumpy19',
     name: 'Paul Grump',
-    avatar_url: 'https://www.tumbit.com/profile-image/4/original/mr-grumpy.jpg',
+    avatar_url: 'https://www.tumbit.com/profile-image/4/original/mr-grumpy.jpg'
   },
   {
     username: 'happyamy2016',
     name: 'Amy Happy',
-    avatar_url: 'https://vignette1.wikia.nocookie.net/mrmen/images/7/7f/Mr_Happy.jpg/revision/latest?cb=20140102171729',
+    avatar_url:
+      'https://vignette1.wikia.nocookie.net/mrmen/images/7/7f/Mr_Happy.jpg/revision/latest?cb=20140102171729'
   },
   {
     username: 'cooljmessy',
     name: 'Peter Messy',
-    avatar_url: 'https://i.imgur.com/WfX0Neu.jpg',
+    avatar_url:
+      'https://scontent.cdninstagram.com/vp/3dbff61874e9e73f7743823f1720b836/5DD20F46/t51.2885-15/e35/s480x480/66072058_533893803815398_5704148835111001089_n.jpg?_nc_ht=instagram.frix2-1.fna.fbcdn.net'
   },
   {
     username: 'weegembump',
     name: 'Gemma Bump',
-    avatar_url: 'https://www.upandrunning.co.uk/media/catalog/product/cache/1/image/650x/040ec09b1e35df139433887a97daa66f/m/r/mr-bump.jpg',
+    avatar_url: 'https://pbs.twimg.com/media/Cvsc7IbWEAEkWGU.jpg'
   },
   {
     username: 'jessjelly',
     name: 'Jess Jelly',
-    avatar_url: 'https://s-media-cache-ak0.pinimg.com/564x/39/62/ec/3962eca164e60cf46f979c1f57d4078b.jpg',
-  },
+    avatar_url:
+      'http://www.femalefirst.co.uk/image-library/square/500/m/mr-tickle-image_08,11.jpg'
+  }
 ];
